24691cf52075994a8c67e0d774ec744854f21ab8
Gareth Fox <gareth@bunguin.games>
2026-05-13 09:12:26 +1200

    Add CI

---
+++ letterboxd-dev-test-senior-main/ci/docker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export HOME=`pwd`
+
+if [ -z "${CI_COMMIT_SHORT_SHA}" ]; then
+  export CI_COMMIT_SHORT_SHA=`git rev-parse --short HEAD`
+fi
+
+export IMAGE=ghcr.io/ayshe/letterboxd-dev-test
+export LATEST=$IMAGE:latest
+export TAG=$IMAGE:$CI_COMMIT_SHORT_SHA
+
+echo Building $TAG
+docker build -t $LATEST -t $TAG .
+docker push $LATEST
+docker push $TAG
